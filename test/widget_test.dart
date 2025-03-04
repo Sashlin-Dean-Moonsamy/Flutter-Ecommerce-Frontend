c4bdbab97559976d667b86148148bb653fd23575
Sashlin Dean Moonsamy <sashlindean.moonsamy@gmail.com>
2025-03-04 15:41:08 +0200

    set test to use app.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,6 +7,66 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import 'package:lux/app.dart';
 
 void main() {
