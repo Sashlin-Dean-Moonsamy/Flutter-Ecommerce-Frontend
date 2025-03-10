6679a6a467ceb8f1131cd9901738152a53cae4f2
Sashlin Dean Moonsamy <sashlindean.moonsamy@gmail.com>
2025-03-10 21:17:41 +0200

    added description

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -1,16 +1,18 @@
 class Product {
   final int id;
   final String name;
+  final String description;
   final double price;
   final String image;
 
-  Product({required this.id, required this.name, required this.price, required this.image});
+  Product({required this.id, required this.name, required this.description, required this.price, required this.image});
 
   factory Product.fromJson(Map<String, dynamic> json) {
     return Product(
       id: json["id"],
       name: json["name"],
-      price: json["price"].toDouble(),
+      description: json["description"],
+      price: double.parse(json["price"].toString()),
       image: json["image"],
     );
   }
